d88d8414b704fccd8fda6bb30352141617120842
Jared Mehle <jrmehle@gmail.com>
2012-03-14 17:01:18 -0500

    Added a new option to apply jQuery UI classes to the input field or not.

--- jquery.combobox.js
+++ jquery.combobox.js
@@ -1,5 +1,8 @@
 $.widget( "ui.combobox", {
-  options: { allowOnlyValid: true },
+  options: {
+    allowOnlyValid: true,
+    attachUiClasses: true
+  },
   _create: function() {
     var self = this;
     var select = this.element.hide(),
@@ -17,39 +20,39 @@ $.widget( "ui.combobox", {
           self.options.open()
         },
         source: function(request, response) {
-          var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-          response( select.children("option" ).map(function() {
-            var text = $( this ).text();
-            if ( this.value && ( !request.term || matcher.test(text) ) )
-              return {
-                label: text.replace(
-                  new RegExp(
-                    "(?![^&;]+;)(?!<[^<>]*)(" +
-                    $.ui.autocomplete.escapeRegex(request.term) +
-                    ")(?![^<>]*>)(?![^&;]+;)", "gi"),
-                  "<strong>$1</strong>"),
-                value: text,
-                option: this
-              };
-          }) );
-        },
-        select: function( event, ui ) {
-          ui.item.option.selected = true;
-          self._trigger( "selected", event, {
-            item: ui.item.option
-          });
-        },
-        change: function(event, ui) {
-          if ( !ui.item ) {
-            var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" ),
-            valid = false;
-            select.children( "option" ).each(function() {
-              if ( this.value.match( matcher ) ) {
-                this.selected = valid = true;
-                return false;
-              }
-            });
-            if ( !valid ) {
+          var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+          response( select.children("option" ).map(function() {
+            var text = $( this ).text();
+            if ( this.value && ( !request.term || matcher.test(text) ) )
+              return {
+                label: text.replace(
+                  new RegExp(
+                    "(?![^&;]+;)(?!<[^<>]*)(" +
+                    $.ui.autocomplete.escapeRegex(request.term) +
+                    ")(?![^<>]*>)(?![^&;]+;)", "gi"),
+                  "<strong>$1</strong>"),
+                value: text,
+                option: this
+              };
+          }) );
+        },
+        select: function( event, ui ) {
+          ui.item.option.selected = true;
+          self._trigger( "selected", event, {
+            item: ui.item.option
+          });
+        },
+        change: function(event, ui) {
+          if ( !ui.item ) {
+            var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" ),
+            valid = false;
+            select.children( "option" ).each(function() {
+              if ( this.value.match( matcher ) ) {
+                this.selected = valid = true;
+                return false;
+              }
+            });
+            if ( !valid ) {
               if(self.options.allowOnlyValid) {
                 // remove invalid value, as it didn't match anything
                 $( this ).val( "" );
@@ -62,39 +65,42 @@ $.widget( "ui.combobox", {
               }
               return false;
             }
-          }
-        }
-      })
-      .addClass("ui-widget ui-widget-content ui-corner-left");
-   
-    input.data( "autocomplete" )._renderItem = function( ul, item ) {
-      return $( "<li></li>" )
-        .data( "item.autocomplete", item )
-        .append( "<a>" + item.label + "</a>" )
-        .appendTo( ul );
-    };
-   
-    $( "<button type=\"button\"> </button>" )
-    .attr( "tabIndex", -1 )
-    .attr( "title", "Show All Items" )
-    .insertAfter( input )
-    .button({
-      icons: {
-        primary: "ui-icon-triangle-1-s"
-      },
-      text: false
-    })
-    .removeClass( "ui-corner-all" )
-    .addClass( "ui-corner-right ui-button-icon" )
-    .click(function() {
-      // close if already visible
-      if (input.autocomplete("widget").is(":visible")) {
-        input.autocomplete("close");
-        return;
-      }
-      // pass empty string as value to search for, displaying all results
-      input.autocomplete("search", "");
-      input.focus();
-    });
-  }
+          }
+        }
+      })
+
+    if(self.options.attachUiClasses) {
+      input.addClass("ui-widget ui-widget-content ui-corner-left");
+    }
+
+    input.data( "autocomplete" )._renderItem = function( ul, item ) {
+      return $( "<li></li>" )
+        .data( "item.autocomplete", item )
+        .append( "<a>" + item.label + "</a>" )
+        .appendTo( ul );
+    };
+
+    $( "<button type=\"button\"> </button>" )
+    .attr( "tabIndex", -1 )
+    .attr( "title", "Show All Items" )
+    .insertAfter( input )
+    .button({
+      icons: {
+        primary: "ui-icon-triangle-1-s"
+      },
+      text: false
+    })
+    .removeClass( "ui-corner-all" )
+    .addClass( "ui-corner-right ui-button-icon" )
+    .click(function() {
+      // close if already visible
+      if (input.autocomplete("widget").is(":visible")) {
+        input.autocomplete("close");
+        return;
+      }
+      // pass empty string as value to search for, displaying all results
+      input.autocomplete("search", "");
+      input.focus();
+   });
+  }
 });
